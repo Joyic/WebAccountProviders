fa98ee398bde1d55f627a8335fdd9bc0da9556cb
Qiongshuo Liang <qilian@microsoft.com>
2016-03-09 15:43:15 -0800

    initial version

---
+++ WebAccountProviders/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+
+namespace WebAccountProviders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetProvider().Wait();
+            Console.ReadKey();
+        }
+
+        private async Task GetProvider()
+        {
+            try
+            {
+                IAsyncOperation<WebAccountProvider> provider = WebAuthenticationCoreManager.FindAccountProviderAsync("https://login.windows.net");//login.windows.local
+                
+
+                //int count = 5;
+
+                //while(provider.Status != AsyncStatus.Completed && count > 0)
+                //{
+                //    Thread.Sleep(200);
+                //    count--;
+                //}
+
+                var result = await provider.AsTask();
+                if (result != null)
+                {
+                    Console.WriteLine(result.DisplayName);
+                    Console.WriteLine(result.DisplayPurpose);
+                    Console.WriteLine(result.Id);
+                }
+                else
+                {
+                    Console.WriteLine("there is no default account is setup for the user");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+        }
+    }
+}
---
+++ WebAccountProviders/WinMDAwaitable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace WebAccountProviders
+{
+    internal static class WinMDAwaitable
+    {
+        internal static Task<T> AsTask<T>(this IAsyncOperation<T> operation, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var registration = cancellationToken.Register(operation.Cancel);
+            var tcs = new TaskCompletionSource<T>();
+            AsyncOperationCompletedHandler<T> completionHandler = (op, status) =>
+            {
+                registration.Dispose();
+                switch (status)
+                {
+                    case AsyncStatus.Canceled:
+                        tcs.TrySetCanceled();
+                        break;
+                    case AsyncStatus.Completed:
+                        tcs.TrySetResult(op.GetResults());
+                        break;
+                    case AsyncStatus.Error:
+                        tcs.TrySetException(op.ErrorCode);
+                        break;
+                    default:
+                        tcs.TrySetException(new InvalidOperationException("Unexpected async operation transition"));
+                        break;
+                }
+            };
+
+            var oldCompleted = operation.Completed;
+            operation.Completed = new AsyncOperationCompletedHandler<T>((op, status) =>
+            {
+                completionHandler(op, status);
+                oldCompleted?.Invoke(op, status);
+            });
+
+            if (operation.Status != AsyncStatus.Started)
+            {
+                completionHandler(operation, operation.Status);
+            }
+
+            return tcs.Task;
+        }
+    }
+}
